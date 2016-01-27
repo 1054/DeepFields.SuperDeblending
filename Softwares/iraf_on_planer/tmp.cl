a618536fd142b518ef1734aca352eb0bf7f2afd3
Daizhong LIU <dzliu@localhost.localdomain>
2016-01-28 05:49:14 +0800

    Update

--- Softwares/iraf_on_planer/tmp.cl
+++ Softwares/iraf_on_planer/tmp.cl
@@ -3,6 +3,8 @@
 # 
 print ''
 imstat tmp.fits[0]
+imstat tmp.fits[1]
 imstat tmp.fits[2]
+imstat tmp.fits[3]
 logout
 
--- Softwares/iraf_on_planer/tmp.sh
+++ Softwares/iraf_on_planer/tmp.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-
-#$ -cwd
-#$ -S /bin/bash
-#$ -V
-
-set -x
-set -e
-
-source /dsm/upgal/data/dliu/Superdeblending/Softwares/SETUP
+#
+set -e  # set this to quit immediately when got any small error
+source /dsm/upgal/data/dliu/Superdeblending/Softwares/SETUP  # this will automatically load IRAF if hostname is planer*
 cd /dsm/upgal/data/dliu/Superdeblending/Softwares/iraf_on_planer/
 cl < tmp.cl
-
 
 
