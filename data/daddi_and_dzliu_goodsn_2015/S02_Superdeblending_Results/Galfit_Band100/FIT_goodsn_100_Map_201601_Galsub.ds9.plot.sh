2a6676dd6caed5fe11fcd92f06c5f642d5078eb2
Daizhong LIU <dzliu@localhost.localdomain>
2016-03-10 02:31:05 +0800

    Update

--- data/daddi_and_dzliu_goodsn_2015/S02_Superdeblending_Results/Galfit_Band100/FIT_goodsn_100_Map_201601_Galsub.ds9.plot.sh
+++
@@ -1,1 +0,0 @@
-ds9mfc FIT_goodsn_100_Map_201601_Galsub.fits -scale limits -1e-6 5e-6 -cmap Heat -zoom to 0.5 -width 1600 -height 600
--- manual/S02_Superdeblending_Manual_dzliu/galsim__copy.sh
+++ manual/S02_Superdeblending_Manual_dzliu/galsim__copy.sh
@@ -1,3 +1,4 @@
+# 2015-12
 cp "/home/dzliu/Data/DeepFields/daddi_goodsn_2015/S02_Superdeblending_FIR/Galsim_Band250_Pass1/sim_diagram_output_v6/dfcorr_1.pdf" "galsim_250_dfcorr_1.pdf"
 cp "/home/dzliu/Data/DeepFields/daddi_goodsn_2015/S02_Superdeblending_FIR/Galsim_Band250_Pass1/sim_diagram_output_v6/dfcorr_2.pdf" "galsim_250_dfcorr_2.pdf"
 cp "/home/dzliu/Data/DeepFields/daddi_goodsn_2015/S02_Superdeblending_FIR/Galsim_Band250_Pass1/sim_diagram_output_v6/dfcorr_3.pdf" "galsim_250_dfcorr_3.pdf"
@@ -18,4 +19,14 @@ cp "/home/dzliu/Data/DeepFields/daddi_goodsn_2015/S02_Superdeblending_FIR/Galsim
 cp "/home/dzliu/Data/DeepFields/daddi_goodsn_2015/S02_Superdeblending_FIR/Galsim_Band500_Pass1/sim_diagram_output_v6/fbias_1.pdf"  "galsim_500_fbias_1.pdf"
 cp "/home/dzliu/Data/DeepFields/daddi_goodsn_2015/S02_Superdeblending_FIR/Galsim_Band500_Pass1/sim_diagram_output_v6/fbias_2.pdf"  "galsim_500_fbias_2.pdf"
 cp "/home/dzliu/Data/DeepFields/daddi_goodsn_2015/S02_Superdeblending_FIR/Galsim_Band500_Pass1/sim_diagram_output_v6/fbias_3.pdf"  "galsim_500_fbias_3.pdf"
+
+
+
+# 2016-03-09
+cp /home/dzliu/Data/DeepFields/daddi_and_dzliu_goodsn_2016/S02_Superdeblending_FIR/Galsim_Band100_CorrectionRecipes_20160129b/sim_diagram_output_v7/galsim_100_*.pdf  .
+cp /home/dzliu/Data/DeepFields/daddi_and_dzliu_goodsn_2016/S02_Superdeblending_FIR/Galsim_Band160_CorrectionRecipes_20160130/sim_diagram_output_v7/galsim_160_*.pdf   .
+cp /home/dzliu/Data/DeepFields/daddi_and_dzliu_goodsn_2016/S02_Superdeblending_FIR/Galsim_Band250_CorrectionRecipes_20160130/sim_diagram_output_v7/galsim_250_*.pdf   .
+cp /home/dzliu/Data/DeepFields/daddi_and_dzliu_goodsn_2016/S02_Superdeblending_FIR/Galsim_Band350_CorrectionRecipes_20160130/sim_diagram_output_v7/galsim_350_*.pdf   .
+cp /home/dzliu/Data/DeepFields/daddi_and_dzliu_goodsn_2016/S02_Superdeblending_FIR/Galsim_Band500_CorrectionRecipes_20160130/sim_diagram_output_v7/galsim_500_*.pdf   .
+cp /home/dzliu/Data/DeepFields/daddi_and_dzliu_goodsn_2016/S02_Superdeblending_FIR/Galsim_Band1160_CorrectionRecipes_20160130/sim_diagram_output_v7/galsim_1160_*.pdf .
 
