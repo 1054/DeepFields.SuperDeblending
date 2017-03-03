e54547fb82e56dd122b2f73e5a8caac12cb60235
1054 <liudz1054@gmail.com>
2017-03-03 18:23:57 +0100

    Updates

--- Softwares/astrodepth_prior_extraction_photometry_example/output_dir/output_name/image_check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-"/Users/dzliu/Cloud/Github/DeepFields.SuperDeblending/Softwares"/CrabFitsImageArithmetic image_sci_input.fits '*' '+1.0' image_sci.fits      -remove-nan
-"/Users/dzliu/Cloud/Github/DeepFields.SuperDeblending/Softwares"/CrabFitsImageArithmetic image_sci_input.fits '*' '-1.0' image_negative.fits -remove-nan
-"/Users/dzliu/Cloud/Github/DeepFields.SuperDeblending/Softwares"/CrabFitsImageArithmetic image_sci.fits '*' 0.0 image_zero.fits -remove-nan
-"/Users/dzliu/Cloud/Github/DeepFields.SuperDeblending/Softwares"/CrabFitsImageArithmetic image_zero.fits '+' 1.0 image_rms.fits -remove-nan
-"/Users/dzliu/Cloud/Github/DeepFields.SuperDeblending/Softwares"/CrabFitsImageArithmetic image_psf_input.fits '*' 1.0 image_psf.fits -remove-nan
-
--- Softwares/astrodepth_prior_extraction_photometry_example/output_dir/output_name/image_psf_peak2total.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-"/Users/dzliu/Cloud/Github/DeepFields.SuperDeblending/Softwares"/CrabPhotImageStatistics image_psf.fits
-
