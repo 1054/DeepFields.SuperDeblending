23d1de5cce8f52dd5e73560768876a4088243a5c
1054 <liudz1054@gmail.com>
2018-02-13 13:06:51 +0100

    Updates

--- Softwares/SETUP.bash
+++ Softwares/SETUP.bash
@@ -5,12 +5,12 @@
 BIN_SETUP_SCRIPT=$(dirname "${BASH_SOURCE[0]}")/bin_setup.bash
 
 if [[ -d "$HOME/Softwares/Supermongo" ]]; then
-    source "$BIN_SETUP_SCRIPT" -var PATH -path "$HOME/Softwares/Supermongo"
+    source "$BIN_SETUP_SCRIPT" -var PATH -path "$HOME/Softwares/Supermongo" -prepend
 fi
 
-if [[ -d "lib_python_dzliu/crabtable" ]]; then
-    source "$BIN_SETUP_SCRIPT" -var PYTHONPATH -path "lib_python_dzliu/crabtable"
-fi
+#if [[ -d "lib_python_dzliu/crabtable" ]]; then
+#    source "$BIN_SETUP_SCRIPT" -var PYTHONPATH -path "lib_python_dzliu/crabtable"
+#fi
 
 source "$BIN_SETUP_SCRIPT" -check gethead getpix sky2xy xy2sky galfit lumdist sm
 
